de14423463b81ceee58d549562d3fd498d1d9660
Pavel Lahoda <pavel.lahoda@bedigi.io>
2024-01-26 08:02:51 +0100

    Initial Commit

---
+++ Geminimalistic/GeminimalisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminimalisticApp.swift
+//  Geminimalistic
+//
+//  Created by Pavel Lahoda on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeminimalisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
